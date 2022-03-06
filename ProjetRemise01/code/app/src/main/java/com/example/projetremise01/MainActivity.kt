f1e1533fcd66e1d51f1dffd8d89fe34a122ba5ff
leomols97 <leomols@hotmail.com>
2022-03-06 20:45:56 +0100

    Remise01 testing

--- ProjetRemise01/code/app/src/main/java/com/example/projetremise01/MainActivity.kt
+++ ProjetRemise01/code/app/src/main/java/com/example/projetremise01/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.projetremise01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.projetremise01.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.connect.setOnClickListener {
+            connect_button(it)
+        }
+    }
+
+    private fun connect_button(view: View?)
+    {
+        val toast = Toast.makeText(applicationContext, "Hello Javatpoint", Toast.LENGTH_SHORT)
+        if (!isValidEmail(binding.mailAdress.toString()))
+            toast.show()
+
+    }
+
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
     }
 }
