711aa61050d4444260b0696acdf5185987163bbb
leomols97 <leomols@hotmail.com>
2022-06-03 20:25:56 +0200

    Uploading a picture works. Now, let's make it correctly

--- ProjetRemise03/LeMenestrel/app/src/main/java/com/example/lemenestrel/FragmentAndVMs/Admin/AdminFragment.kt
+++ ProjetRemise03/LeMenestrel/app/src/main/java/com/example/lemenestrel/FragmentAndVMs/Admin/AdminFragment.kt
@@ -2,8 +2,12 @@ package com.example.lemenestrel.FragmentAndVMs.Admin
 
 import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.app.ProgressDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,8 +22,10 @@ import com.example.lemenestrel.databinding.FragmentAdminBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.net.URI
 
 class AdminFragment : Fragment() {
 
@@ -36,6 +42,7 @@ class AdminFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_admin, container, false)
+//        uploadPictureProcess()
 
         return binding.root
     }
@@ -48,16 +55,62 @@ class AdminFragment : Fragment() {
         binding.authButton.setOnClickListener { launchSignInFlow() }
     }
 
-    val ImageBack = 1
+    // To make the picture upload possible
+    val IMAGE_BACK = 1
     lateinit var storage: StorageReference
     private fun uploadPicture() {
         storage = FirebaseStorage.getInstance().reference.child("PictureFolder")
         binding.uploadPicture.setOnClickListener {
             val intent = Intent(Intent.ACTION_GET_CONTENT)
-            intent.setType("pictures/*")
-            startActivityForResult(intent, ImageBack)
+            intent.setType("image/*")
+            startActivityForResult(intent, IMAGE_BACK)
         }
     }
+
+//    lateinit var pictureURI: Uri
+//    private fun uploadPictureProcess() {
+//        binding.selectPicture.setOnClickListener{
+////            selectPicture()
+//            openGalleryForImage()
+//        }
+//        binding.uploadPicture.setOnClickListener{
+//            uploadPicture()
+//        }
+//    }
+
+//    private fun selectPicture() {
+//        val intent = Intent(Intent.ACTION_PICK)
+//        intent.type = "image/*"
+//        intent.action = Intent.ACTION_GET_CONTENT
+//        startActivityForResult(intent, 1000)
+//    }
+
+//    private fun openGalleryForImage() {
+//        val intent = Intent(Intent.ACTION_PICK)
+//        intent.type = "image/*"
+//        startActivityForResult(intent, 100)
+//    }
+
+//    private fun uploadPicture() {
+//        val progressDialog = ProgressDialog(activity)
+//        progressDialog.setMessage("Uploading picture...")
+//        progressDialog.setCancelable(true)
+//        progressDialog.show()
+//
+//        val fileName = "testUploading"
+//        val storageReference = FirebaseStorage.getInstance().getReference("images/$fileName")
+//
+//        storageReference.putFile(pictureURI).addOnSuccessListener {
+//            binding.imageView.setImageURI(null)
+//            Toast.makeText(requireActivity(), "Picture successfully uploaded !", Toast.LENGTH_SHORT).show()
+//            if (progressDialog.isShowing)
+//                progressDialog.dismiss()
+//        }.addOnFailureListener {
+//            if (progressDialog.isShowing)
+//                progressDialog.dismiss()
+//            Toast.makeText(requireActivity(), "Picture NOT uploaded !", Toast.LENGTH_SHORT).show()
+//        }
+//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -66,8 +119,7 @@ class AdminFragment : Fragment() {
             if (resultCode == Activity.RESULT_OK) {
                 // User successfully signed in
                 Log.i(
-                    TAG,
-                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                    TAG,"Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
                 )
             } else {
                 // Sign in failed. If response is null the user canceled the
@@ -75,12 +127,20 @@ class AdminFragment : Fragment() {
                 // response.getError().getErrorCode() and handle the error.
                 Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
             }
-            // To make the picture upload possible
+//            if (requestCode == 100 && resultCode == RESULT_OK ) {
+//                pictureURI = data?.data!!
+//                binding.imageView.setImageURI(pictureURI) // handle chosen image
+//            }
+        }
+        // To make the picture upload possible
+        if (requestCode == IMAGE_BACK) {
             if (resultCode == RESULT_OK) {
                 val pictureData = data!!.getData()
-                val pictureName: StorageReference = storage.child("picture" + pictureData!!.lastPathSegment)
+                val pictureName: StorageReference = storage.child("image" + pictureData!!.lastPathSegment)
                 pictureName.putFile(pictureData).addOnSuccessListener { taskSnapShot ->
                     Toast.makeText(requireActivity(), "Picture uploaded", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Toast.makeText(requireActivity(), "Picture NOT uploaded", Toast.LENGTH_SHORT).show()
                 }
             }
         }
